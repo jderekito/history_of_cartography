d1e9b42d48922639b0db82d405ae809abfc04688
jderekito <johnderekmorgan@hotmail.com>
2016-08-21 09:23:13 -0500

    init commit
    init

---
+++ carto_hist_places.js
@@ -0,0 +1,89 @@
+var bldgData = {
+	"features" : [{
+			"geometry" : { 
+				"coordinates" : [43.679291, 33.223191],
+				"type" : "Point"
+			},
+			"id" : "1",
+			"properties" : {
+				"NAME" : "600 BC: First world map",
+				"BODY" : "Babylonians believed to the first to make a map or attempt to map their world. This map is credited to Anaximander credited with invention of map-drawing.",
+                "IMAGE" : "firstworldmap.jpg"
+			},
+			"type" : "Feature"
+		}, {
+			"geometry" : {
+				"coordinates" : [23.727539, 37.983810],
+				"type" : "Point"
+			},
+			"id" : "2",
+			"properties" : {
+				"NAME" : "384-322 BC: Aristotle on a spherical earth",
+				"BODY" : "Aristotle believes earth's sphericity can be proven through visual observation.",
+                "IMAGE" : "aristotle.jpg"
+			},
+			"type" : "Feature"
+		}, {
+			"geometry" : {
+				"coordinates" : [29.918739, 31.200092],
+				"type" : "Point"
+			},
+			"id" : "3",
+			"properties" : {
+				"NAME" : "300 BC: Euclid writes Elements",
+				"BODY" : "With Euclid's Elements around 300 BC you have the introduction of a math system (Geometry) which would be built upon by others to make specific discoveries pivotal to cartographic thought. For instance Eratosthenes would use the Euclid’s to approximate to within 14-19% of accuracy the circumference of the earth.",
+                "IMAGE" : "euclid.jpg"
+			},
+			"type" : "Feature"
+		},  {
+			"geometry" : {
+				"coordinates" : [32.899829, 24.088938],
+				"type" : "Point"
+			},
+			"id" : "4",
+			"properties" : {
+				"NAME" : "276-195 BC: Eratosthenes",
+				"BODY" : "Eratosthenes makes first scientific estimate of earth's circumference",
+                "IMAGE" : "eratosthenes.jpg"
+			},
+			"type" : "Feature"
+		},  {
+			"geometry" : {
+				"coordinates" : [29.918739, 31.200092],
+				"type" : "Point"
+			},
+			"id" : "5",
+			"properties" : {
+				"NAME" : "150-125 BC: Ptolemy",
+				"BODY" : "Ptolemy first to apply the scientific principles to the idea of mapping in a book called Geography… put in to a book the ideas of scale and other important cartographic principles …. Archived at the library in Alexandria, Egypt, which was burned 391 AD by Christian mobs… around this time we have the start of what is often referred to as the middle ages",
+                "IMAGE" : "ptolemy.jpg"
+			},
+			"type" : "Feature"
+		},  {
+			"geometry" : {
+				"coordinates" : [-5.984459, 37.389092],
+				"type" : "Point"
+			},
+			"id" : "6",
+			"properties" : {
+				"NAME" : "600-1400 AD: T-O Maps",
+				"BODY" : "Isidore of Seville, a bishop in Spain where The T is the Mediterranean, dividing the three continents, Asia, Europe and Africa, and the O is the surrounding Ocean. Jerusalem was generally represented in the center of the map. Asia was typically the size of the other two continents combined.",
+                "IMAGE" : "t_o_map.jpg"
+			},
+			"type" : "Feature"  
+		},  {
+			"geometry" : {
+				"coordinates" : [12.315515, 45.440847],
+				"type" : "Point"
+			},
+			"id" : "7",
+			"properties" : {
+				"NAME" : "1300 AD: Portolan Charts",
+				"BODY" : "in the 1300s Italian geographer Pietro Vesconte pioneers the usage of portolan charts, which are navigational maps based on compass directions and estimated distances observed by the pilots at sea.",
+                "IMAGE" : "portolan.jpg"
+			},
+			"type" : "Feature"
+		}           
+	],
+	"type" : "FeatureCollection"
+};
